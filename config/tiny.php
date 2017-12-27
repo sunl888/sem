ec3931d73598f20334fccc054f477a1baec0c8c1
2013855675@qq.com <2013855675@qq.com>
2017-12-27 15:11:40 +0800

    fixd bug

--- config/tiny.php
+++ config/tiny.php
@@ -5,7 +5,7 @@ return [
     'default_user_password' => 'test1234',
 
     'max_per_page' => 40,
-    'default_per_page' => 15,
+    'default_per_page' => 8,
 
     'default_slug_mode' => 'pinyin',
 
